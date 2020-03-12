ce3ac05239df7f6506828fc655b6f439ed50218b
ccrum292 <ccrum292@gmail.com>
2020-03-12 14:46:36 -0400

    updating

--- server.js
+++ server.js
@@ -31,6 +31,63 @@ connection.connect(function(err){
   console.log("connected as id " + connection.threadId);
 });
 
+app.get("/", function(req, res){
+
+  connection.query("SELECT * FROM burgers", function(err, results){
+    if (err) {
+      return res.status(500).end();
+    }
+
+    const burgersM = results.filter(val=>{
+      return !val.devoured;
+    });
+
+    const burgersE = results.filter(val=>{
+      return val.devoured;
+    })
+
+    res.render("index", { burgersMade: burgersM, burgersEaten: burgersE })
+
+  });
+
+});
+
+app.post("/api/burgers", function(req, res){
+
+  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, results){
+    if(err){
+      res.status(500).end();
+    };
+
+    res.json({ id: results.insertId });
+    console.log({ id: results.insertId });
+
+  });
+
+});
+
+
+// ?????
+app.put("/api/burgers/:id", function(req, res){
+  console.log(req.params.id);
+  connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [req.params.id], function(err, results) {
+    console.log(results)
+    if (err){
+      return res.status(500).end();
+    }else if(results.changedRows === 0){
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+
+});
+
+
+
+
+// selectAll();
+// insertOne();
+// updateOne();
 
 app.listen(PORT, function(){
   console.log("Server listening on: http://localhost:"+ PORT);
