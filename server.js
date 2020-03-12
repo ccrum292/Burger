bd97b0fdf7231c16a825e6b3add6072f99cc873e
ccrum292 <ccrum292@gmail.com>
2020-03-12 15:21:02 -0400

    heroku

--- server.js
+++ server.js
@@ -13,14 +13,19 @@ app.set("view engine", "handlebars");
 
 var PORT = process.env.PORT || 8080;
 
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+};
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
 
 connection.connect(function(err){
   if(err){
